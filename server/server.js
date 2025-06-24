8eba253d145e2f04f5f1fe4d215c5c1f35fefb3b
hadas3252@outlook.co.il <hadas3252@outlook.co.il>
2025-06-24 23:32:57 +0300

    wow

--- server/server.js
+++ server/server.js
@@ -11,7 +11,11 @@ const app = express();
 
 // Middlewares
 const corsOptions = {
-  origin: 'http://localhost:5173', 
+   origin: [
+    'http://localhost:5173',
+    'http://localhost:5175',
+    'http://localhost:5176'
+  ], 
   credentials: true,               // מאפשר שליחת cookies
 };
 app.use(cors(corsOptions));
