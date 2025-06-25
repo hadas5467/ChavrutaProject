4c1568a0a7988a310837debc13f0d37ed7a096e9
hadas3252@outlook.co.il <hadas3252@outlook.co.il>
2025-06-25 11:43:09 +0300

    BRACHI

---
+++ server/Middleware/multer.js
@@ -0,0 +1,35 @@
+import multer from 'multer';
+import path from 'path';
+const allowedExts = ['.jpg', '.jpeg', '.png', '.gif', '.webp'];
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+  const uploadPath = 'uploads/';
+  if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+  }
+  cb(null, uploadPath);
+},
+
+filename: (req, file, cb) => {
+  const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+  const ext = path.extname(file.originalname);
+  cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+}
+
+
+});
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 2 * 1024 * 1024 }, // עד 2MB
+ fileFilter: (req, file, cb) => {
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (file.mimetype.startsWith('image/') && allowedExts.includes(ext)) {
+    cb(null, true);
+  } else {
+    cb(new Error('Only image files are allowed'), false);
+  }
+}
+});
+
+export default upload;
